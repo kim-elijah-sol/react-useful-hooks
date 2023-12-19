95cf4a0f91a2666c6a001f7a4843d2fea5549c3e
kim-elijah-sol <kimelijahsol@gmail.com>
2023-12-19 22:30:10 +0900

    fix: [useTimeout] useIsomorphicLayoutEffect 으로 대체

--- src/useTimeout.ts
+++ src/useTimeout.ts
@@ -1,17 +1,13 @@
 import { useEffect, useRef } from 'react'
-import { useIsMounted } from 'useIsMounted'
+import { useIsomorphicLayoutEffect } from 'useIsomorphicLayoutEffect'
 
 export function useTimeout(fn: () => void, delay?: number): void
 export function useTimeout(fn: () => void, delay = 300) {
   const savedFn = useRef<() => void>(fn)
 
-  const isMounted = useIsMounted()
-
-  useEffect(() => {
-    if (isMounted) {
-      savedFn.current = fn
-    }
-  }, [isMounted, fn])
+  useIsomorphicLayoutEffect(() => {
+    savedFn.current = fn
+  }, [fn])
 
   useEffect(() => {
     const timer = setTimeout(() => {
